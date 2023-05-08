a02c8bf61c45b80cc150b874a5fa9418e979f88b
yuto90 <solidusgako4087@gmail.com>
2023-05-08 22:18:42 +0900

    useEffect

--- components/Headline.tsx
+++ components/Headline.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { ReactElement, useCallback } from "react";
+import { ReactElement } from "react";
 import Link from "next/link";
 
 interface Props {
@@ -8,13 +8,13 @@ interface Props {
 }
 // propsはオブジェクトで渡ってくる
 export function Headline(props: Props) {
-  // Reactが提供する型定義で、<a> タグのクリックイベントに適用される型
-  // useCallback: コンポーネントが再レンダリングされても関数は再生成されなくなる
-  const handleClick = useCallback((e: React.MouseEvent<HTMLAnchorElement>) => {
-    console.log(e.target);
-    // 画面がリロードされてしまうのを止める
-    e.preventDefault()
-  }, []);
+  // // Reactが提供する型定義で、<a> タグのクリックイベントに適用される型
+  // // useCallback: コンポーネントが再レンダリングされても関数は再生成されなくなる
+  // const handleClick = useCallback((e: React.MouseEvent<HTMLAnchorElement>) => {
+  //   console.log(e.target);
+  //   // 画面がリロードされてしまうのを止める
+  //   e.preventDefault()
+  // }, []);
 
   return (
     <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -27,9 +27,9 @@ export function Headline(props: Props) {
         <br />
         <Link href="/about">About</Link>
         <br />
-        <a href="" onClick={handleClick}>
+        {/* <a href="" onClick={handleClick}>
           ボタン
-        </a>
+        </a> */}
       </div>
 
       <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,19 @@
+import { useEffect } from "react";
 import { Main } from "../components/Main";
 
 export default function index() {
+
+  // コンポーネントのマウント時に一度だけ実行
+  useEffect(() => { 
+    console.log('マウント時');
+    // マウント時に背景色を変える
+    document.body.style.backgroundColor = "lightblue";
+
+    // アンマウント時にクリーンアップ関数を使って背景色を戻す
+    return () => {
+    console.log('アンマウント時');
+      document.body.style.backgroundColor = "";
+    };
+  }, [])
   return <Main page="index" />;
 }
